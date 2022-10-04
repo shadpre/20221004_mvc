ce58371c8d74b716ab3673ec4e35ea7a3bd87c66
shadpre <klavs@tranborg.eu>
2022-10-04 10:17:14 +0200

    Init

---
+++ src/main/java/com/edencoding/controllers/UserInfoViewController.java
@@ -0,0 +1,20 @@
+package com.edencoding.controllers;
+
+import com.edencoding.models.FinancialAccount;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserInfoViewController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
